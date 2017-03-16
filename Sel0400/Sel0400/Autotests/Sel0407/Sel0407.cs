89fb2e07944ad488f48510ab7f35b25b1e67b9e9
m.vertebnyi <m.vertebnyi@plarium.com>
2017-03-16 12:56:07 +0200

    [x] Задание 7. Сделайте сценарий, проходящий по всем разделам админки
    Добавлен тест

---
+++ Sel0400/Sel0400/Autotests/Sel0407/Sel0407.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using Sel0400.Framework;
+using Sel0400.Framework.Constants;
+
+namespace Sel0400.Autotests.Sel0407
+{
+  class Sel0407
+  {
+    public static void Test01_Admin_OpenAllMenuAndsubmenu()
+    {
+      var driver = TestBase.Select_driver();
+      driver.Navigate().GoToUrl(Urls.Admin);
+      driver.FindElement(By.Name("username")).SendKeys("admin");
+      driver.FindElement(By.Name("password")).SendKeys("admin");
+      driver.FindElement(By.Name("login")).Click();
+      IWebElement messageSuccessElement = driver.FindElement(By.ClassName("success"));
+      Thread.Sleep(2000);
+      if (!messageSuccessElement.Displayed)
+      {
+        driver.FindElement(By.Name("password")).SendKeys(Keys.Enter);
+      }
+      Thread.Sleep(2000);
+      //ReadOnlyCollection<IWebElement> listMenu = driver.FindElements(By.XPath(".//*[@id='app-']/a/span[2]"));
+      //ReadOnlyCollection<IWebElement> listSubMenu = driver.FindElements(By.XPath(".//*[@id='app-']/ul/li"));
+      for (int i = 0; i < driver.FindElements(By.XPath(".//*[@id='app-']/a/span[2]")).Count; i++)
+      {
+        driver.FindElements(By.XPath(".//*[@id='app-']/a/span[2]"))[i].Click();
+        Console.WriteLine("Menu {0}:", driver.FindElements(By.XPath(".//*[@id='app-']/a/span[2]"))[i]);
+        for (int j = 0; j < driver.FindElements(By.XPath(".//*[@id='app-']/ul/li")).Count; j++)
+        {
+          driver.FindElements(By.XPath(".//*[@id='app-']/ul/li"))[j].Click();
+          Console.WriteLine("Sub-menu: {0}", driver.FindElements(By.XPath(".//*[@id='app-']/ul/li"))[j].Text);
+          if (driver.FindElement(By.TagName("h1")).Displayed)
+          {
+            Console.WriteLine("h1 tag found, text:{0}", driver.FindElement(By.TagName("h1")).Text);
+          }
+          else
+          {
+            Console.WriteLine("h1 tag not found");
+          }
+        }
+      }
+      Thread.Sleep(5000);
+
+      TestBase.CloseDriver();
+    }
+  }
+}
--- Sel0400/Sel0400/Program.cs
+++ Sel0400/Sel0400/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Sel0400.Autotests.Sel0401;
 using Sel0400.Autotests.Sel0402;
+using Sel0400.Autotests.Sel0407;
 
 namespace Sel0400
 {
@@ -8,7 +9,8 @@ namespace Sel0400
   {
     static void Main()
     {
-      Console.WriteLine("Please select Autotests: \n1 - [x] Задание 1. Подготовьте инфраструктуру \n2 - [x] Задание 3. Сделайте сценарий логина");
+      Console.WriteLine("Please select Autotests: \n1 - [x] Задание 1. Подготовьте инфраструктуру \n" +
+                        "2 - [x] Задание 3. Сделайте сценарий логина \n7 - [x] Задание 7. Сделайте сценарий, проходящий по всем разделам админки");
       string selectNimber = Console.ReadLine();
       switch (selectNimber)
       {
@@ -17,6 +19,9 @@ namespace Sel0400
           break;
         case "2":
           Sel0402.Test01_Admin_LoginAndVerify();
+          break;
+        case "7":
+          Sel0407.Test01_Admin_OpenAllMenuAndsubmenu();
           break;
         default:
           Console.WriteLine("Incorrect inputnumber");
