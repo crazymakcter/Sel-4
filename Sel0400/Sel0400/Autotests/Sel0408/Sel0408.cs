8794d2cfd2cf3113ff8bf36997e1c77d24f2c4a2
m.vertebnyi <m.vertebnyi@plarium.com>
2017-03-16 13:04:27 +0200

    [x] Задание 8. Сделайте сценарий, проверяющий наличие стикеров у товаров

---
+++ Sel0400/Sel0400/Autotests/Sel0408/Sel0408.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using OpenQA.Selenium;
+using Sel0400.Framework;
+using Sel0400.Framework.Constants;
+
+namespace Sel0400.Autotests.Sel0408
+{
+  class Sel0408
+  {
+    public static void Test01_HomePage_VerifyLabelOnProduc()
+    {
+      var driver = TestBase.Select_driver();
+      driver.Navigate().GoToUrl(Urls.HomePage);
+      IList<IWebElement> listProducts = driver.FindElements(By.CssSelector(".product.column.shadow.hover-light"));
+      for (int i = 0; i < listProducts.Count; i++)
+      {
+
+        try
+        {
+          if (listProducts[i].FindElement(By.CssSelector(".sticker")).Displayed)
+          {
+            if (listProducts[i].FindElements(By.CssSelector(".sticker")).Count > 1)
+            {
+              Console.WriteLine("{0} - have more than 1 sticker",
+                  listProducts[i].Text);
+            }
+            else
+            {
+              Console.WriteLine("{0} - have sticker {1}",
+                  listProducts[i].Text,
+                  listProducts[i].FindElement(By.CssSelector(".sticker")).Text);
+            }
+          }
+        }
+        catch
+        {
+          Console.WriteLine("{0} - not have sticker",
+              listProducts[i].Text);
+        }
+      }
+
+      Console.ReadKey();
+      driver.Quit();
+    }
+  }
+}
--- Sel0400/Sel0400/Program.cs
+++ Sel0400/Sel0400/Program.cs
@@ -2,6 +2,7 @@
 using Sel0400.Autotests.Sel0401;
 using Sel0400.Autotests.Sel0402;
 using Sel0400.Autotests.Sel0407;
+using Sel0400.Autotests.Sel0408;
 
 namespace Sel0400
 {
@@ -10,7 +11,9 @@ namespace Sel0400
     static void Main()
     {
       Console.WriteLine("Please select Autotests: \n1 - [x] Задание 1. Подготовьте инфраструктуру \n" +
-                        "2 - [x] Задание 3. Сделайте сценарий логина \n7 - [x] Задание 7. Сделайте сценарий, проходящий по всем разделам админки");
+                        "2 - [x] Задание 3. Сделайте сценарий логина \n" +
+                        "7 - [x] Задание 7. Сделайте сценарий, проходящий по всем разделам админки \n" +
+                        "8 - [x] Задание 8. Сделайте сценарий, проверяющий наличие стикеров у товаров");
       string selectNimber = Console.ReadLine();
       switch (selectNimber)
       {
@@ -22,6 +25,9 @@ namespace Sel0400
           break;
         case "7":
           Sel0407.Test01_Admin_OpenAllMenuAndsubmenu();
+          break;
+        case "8":
+          Sel0408.Test01_HomePage_VerifyLabelOnProduc();
           break;
         default:
           Console.WriteLine("Incorrect inputnumber");
